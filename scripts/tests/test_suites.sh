8fbb911390e5639165c40c12ebffbb318ab287bd
Boris Zbarsky <bzbarsky@apple.com>
2021-07-17 03:08:02 -0400

    Testing stuff

--- scripts/tests/test_suites.sh
+++ scripts/tests/test_suites.sh
@@ -86,6 +86,7 @@ for j in "${iter_array[@]}"; do
     echo " ===== Iteration $j starting"
     for i in "${test_array[@]}"; do
         echo "  ===== Running test: $i"
+        netstat -m
         echo "          * Starting cluster server"
         rm -rf /tmp/chip_tool_config.ini
         # This part is a little complicated.  We want to
